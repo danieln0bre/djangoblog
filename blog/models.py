73e8e87d6be1e070a271a5f3d7b99ecf00cac74e
daniel <danielnobre09@gmail.com>
2023-08-08 11:51:13 -0300

    first models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=25, blank=False, null=False)
+    last_name = models.CharField(max_length=25, blank=False, null=False)
+    email = models.EmailField(blank=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, blank=False, null=False)
+    excerpt = models.CharField(max_length=200, blank=False, null=False)
+    image_name = models.CharField(max_length=100, blank=False, null=False)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+
+class Tag(models.Model):
